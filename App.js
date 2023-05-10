086d90e1e95b932ada6846bdeb63990e8d99652e
maxg1007 <90800419+maxg1007@users.noreply.github.com>
2023-05-10 17:07:42 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import StackNavigator from "./stack";
+import { NavigationContainer } from "@react-navigation/native";
+import * as Font from "expo-font";
+import { firebaseConfig } from "./config";
+import firebase from "firebase";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+let customFonts = {
+  "Bubblegum-Sans": require("./assets/BubblegumSans-Regular.ttf"),
+};
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fontLoade: false,
+    };
+  }
+
+  loadFont = async () => {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontLoade: true });
+  };
+  componentDidMount() {
+    this.loadFont();
+  }
+  render() {
+    if (this.state.fontLoade) {
+      return (
+        <NavigationContainer>
+          <StackNavigator></StackNavigator>
+        </NavigationContainer>
+      );
+    }
+    return null;
+  }
+}
